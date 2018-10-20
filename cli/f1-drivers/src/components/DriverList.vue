7e19deadd740df8d8dae681c9094e47918e11d3b
Marcin Gościniak <marcingosciniak@poczta.fm>
2018-10-20 14:43:39 +0200

    Revert "Revert "[#2.2] move data to store""
    This reverts commit 2714ea150b9750f49d81d45625a2c50ce9f490e9.

--- cli/f1-drivers/src/components/DriverList.vue
+++ cli/f1-drivers/src/components/DriverList.vue
@@ -2,10 +2,9 @@
   <div class="driver-list">
     <h2>F1 drivers</h2>
     <ul>
-      <DriverListItem v-for="(driver, index) in drivers" 
-        :key="index" 
+      <DriverListItem v-for="driver in sharedState.drivers" 
+        :key="driver.id" 
         :driver="driver"
-        :index="index" 
         @remove-driver="removeDriver" />
     </ul>
       <NewDriverForm @add-driver="addDriver" />
@@ -15,28 +14,27 @@
 <script>
 import DriverListItem from './DriverListItem';
 import NewDriverForm from './NewDriverForm';
+import store from '../store';
 
 export default {
   components: {
     DriverListItem,
     NewDriverForm
   },
+  created() {
+    store.fetchDrivers();
+  },
   data () {
     return {
-      drivers: [
-        {
-          firstname: "Max",
-          lastname: "Verstappen"
-        }
-      ]
+      sharedState: store.state 
     }
   },
   methods: {
     addDriver(driver) {
-      this.drivers.push(driver);
+      store.addDriver(driver);
     },
-    removeDriver(index) {
-      this.drivers.splice(index, 1);
+    removeDriver(id) {
+      store.removeDriver(id);
     }
   },
   name: 'DriverList'
