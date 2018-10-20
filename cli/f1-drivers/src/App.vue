d7e8856a828e71ee9d26043b3cfcc2afbfed3073
Marcin Gościniak <marcingosciniak@poczta.fm>
2018-10-20 11:10:54 +0200

    [#2.1] move feature to CLI-based project

---
+++ cli/f1-drivers/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Drivers />
+  </div>
+</template>
+
+<script>
+import Drivers from './components/Drivers.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Drivers,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ cli/f1-drivers/src/components/Drivers.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="drivers">
+    <h2>F1 drivers</h2>
+    <ul>
+      <li v-for="(driver, index) in drivers" :key="index">
+          {{`${driver.firstname} ${driver.lastname}`}}
+          <button class="inline-button" type="button" v-on:click="removeDriver(index)">Remove</button>
+      </li>
+    </ul>
+
+    <input type="text" v-model="newDriverFirstname" />
+    <input type="text" v-model="newDriverLastname" />
+    <input type="button" value="Add" v-on:click="addDriver" />
+    <div v-if="unableToAdd">
+        Fill both first name and last name input.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drivers: [
+        {
+          firstname: "Max",
+          lastname: "Verstappen"
+        }
+      ],
+      newDriverFirstname: '',
+      newDriverLastname: '',
+      unableToAdd: false
+    }
+  },
+  methods: {
+    addDriver() {
+      this.clearMessage();
+
+      if (!!this.newDriverFirstname && !!this.newDriverLastname) {
+          this.drivers.push({
+              firstname: this.newDriverFirstname,
+              lastname: this.newDriverLastname
+          });
+          this.newDriverFirstname = '';
+          this.newDriverLastname = '';
+      } else {
+          this.unableToAdd = true;
+      }
+    },
+    removeDriver(index) {
+        this.drivers.splice(index, 1)
+    },
+    clearMessage() {
+        this.unableToAdd = false;
+    }
+  },
+  name: 'Drivers'
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
